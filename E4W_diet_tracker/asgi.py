312e637c22ac35bfb3b1ee4e43157662fb16bfc3
Reise Campbell <54416757+GarlicDebug@users.noreply.github.com>
2020-12-01 15:03:58 -0500

    Initial commit

---
+++ E4W_diet_tracker/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for E4W_diet_tracker project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'E4W_diet_tracker.settings')
+
+application = get_asgi_application()
