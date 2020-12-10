6996a08cb35531f523337765b222eee23decb23c
Reise Campbell <54416757+GarlicDebug@users.noreply.github.com>
2020-12-09 21:25:00 -0500

    Added user-associated meal-logging

--- meal_log/models.py
+++ meal_log/models.py
@@ -1,8 +1,9 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 
-# Create your models here.
 class Meal(models.Model):
+    user = models.ForeignKey(User, models.SET_NULL, null=True)
     date = models.TextField()
     time = models.TextField()
     description = models.TextField()
--- meal_log/views.py
+++ meal_log/views.py
@@ -1,20 +1,21 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 from .models import Meal
 
 
 # Create your views here.
 def meal_logView(request):
-    all_meals = Meal.objects.all()
+    all_meals = Meal.objects.filter(user=request.user)
     return render(request, 'meal_log.html',
                   {'all_items': all_meals})
 
 
 def addMeal(request):
     form = request.POST
-    new_meal = Meal(date=form['date'], time=form['time'], description=form['description'])
+    new_meal = Meal(user=request.user, date=form['date'], time=form['time'], description=form['description'])
     new_meal.save()
     return HttpResponseRedirect('/meallog/')
+
 
 def deleteMeal(request, todo_id):
     meal_to_delete = Meal.objects.get(id=todo_id)
